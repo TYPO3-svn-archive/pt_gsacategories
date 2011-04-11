7a22539e5a09e83cff836b6e5729190d5207cf0a
fabriziobranca <fabriziobranca@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-11 16:20:14 +0000

    Fixed deprecation log messages
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pt_gsacategories/trunk@46445 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -4,7 +4,7 @@ if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 t3lib_div::loadTCA('tt_content');
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1'] = 'layout,select_key,pages,recursive';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1'] = 'pi_flexform';
-t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/pi1/flexform_ds.xml'); 
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/pi1/flexform_ds.xml');
 
 
 t3lib_extMgm::addPlugin(array('LLL:EXT:pt_gsacategories/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
@@ -14,7 +14,11 @@ t3lib_extMgm::addStaticFile($_EXTKEY,'static/catDataProvider/','GSA Categories: 
 t3lib_extMgm::addStaticFile($_EXTKEY,'static/catmenu/','GSA Categories: Category Menu');
 
 // add folder type and icon
-$ICON_TYPES['gsacat'] = array('icon' => t3lib_extMgm::extRelPath($_EXTKEY).'icon_tx_ptgsacategories_sysfolder_cat.png');
+if (t3lib_div::compat_version('4.4')) {
+	t3lib_SpriteManager::addTcaTypeIcon('pages', 'contains-gsacat', '../typo3conf/ext/pt_gsacategories/icon_tx_ptgsacategories_sysfolder_cat.png');
+} else {
+	$ICON_TYPES['gsacat'] = array('icon' => t3lib_extMgm::extRelPath($_EXTKEY).'icon_tx_ptgsacategories_sysfolder_cat.png');
+}
 $TCA['pages']['columns']['module']['config']['items'][] = array('LLL:EXT:pt_gsacategories/locallang_db.xml:tx_ptgsacategories_cat', 'gsacat');
 
 
@@ -22,16 +26,16 @@ t3lib_extMgm::allowTableOnStandardPages('tx_ptgsacategories_cat');
 
 $TCA['tx_ptgsacategories_cat'] = array (
 	'ctrl' => array (
-		'title'     => 'LLL:EXT:pt_gsacategories/locallang_db.xml:tx_ptgsacategories_cat',		
-		'label'     => 'title',	
+		'title'     => 'LLL:EXT:pt_gsacategories/locallang_db.xml:tx_ptgsacategories_cat',
+		'label'     => 'title',
 		'tstamp'    => 'tstamp',
 		'crdate'    => 'crdate',
 		'cruser_id' => 'cruser_id',
-		'default_sortby' => 'ORDER BY crdate',	
-		'delete' => 'deleted',	
-		'enablecolumns' => array (		
-			'disabled' => 'hidden',	
-			'starttime' => 'starttime',	
+		'default_sortby' => 'ORDER BY crdate',
+		'delete' => 'deleted',
+		'enablecolumns' => array (
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
 			'endtime' => 'endtime',
 		),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca_pt_gsacategories.php',
@@ -47,17 +51,17 @@ t3lib_extMgm::allowTableOnStandardPages('tx_ptgsacategories_cat_art_rel');
 
 $TCA['tx_ptgsacategories_cat_art_rel'] = array (
 	'ctrl' => array (
-		'title'     => 'LLL:EXT:pt_gsacategories/locallang_db.xml:tx_ptgsacategories_cat_art_rel',		
-		'label'     => 'uid',	
+		'title'     => 'LLL:EXT:pt_gsacategories/locallang_db.xml:tx_ptgsacategories_cat_art_rel',
+		'label'     => 'uid',
 		'tstamp'    => 'tstamp',
 		'crdate'    => 'crdate',
 		'cruser_id' => 'cruser_id',
 		'hideTable' => true,
-		'default_sortby' => 'ORDER BY crdate',	
-		'delete' => 'deleted',	
-		'enablecolumns' => array (		
-			'disabled' => 'hidden',	
-			'starttime' => 'starttime',	
+		'default_sortby' => 'ORDER BY crdate',
+		'delete' => 'deleted',
+		'enablecolumns' => array (
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
 			'endtime' => 'endtime',
 		),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca_pt_gsacategories.php',
@@ -73,17 +77,17 @@ t3lib_extMgm::allowTableOnStandardPages('tx_ptgsacategories_cat_cat_rel');
 
 $TCA['tx_ptgsacategories_cat_cat_rel'] = array (
 	'ctrl' => array (
-		'title'     => 'LLL:EXT:pt_gsacategories/locallang_db.xml:tx_ptgsacategories_cat_cat_rel',		
-		'label'     => 'uid',	
+		'title'     => 'LLL:EXT:pt_gsacategories/locallang_db.xml:tx_ptgsacategories_cat_cat_rel',
+		'label'     => 'uid',
 		'tstamp'    => 'tstamp',
 		'crdate'    => 'crdate',
 		'cruser_id' => 'cruser_id',
 		'hideTable' => true,
-		'default_sortby' => 'ORDER BY crdate',	
-		'delete' => 'deleted',	
-		'enablecolumns' => array (		
-			'disabled' => 'hidden',	
-			'starttime' => 'starttime',	
+		'default_sortby' => 'ORDER BY crdate',
+		'delete' => 'deleted',
+		'enablecolumns' => array (
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
 			'endtime' => 'endtime',
 		),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca_pt_gsacategories.php',
